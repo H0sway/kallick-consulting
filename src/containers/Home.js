27af6490cf19b6f48bd6699559ea2dc21b9b0e15
Joshua Russell <jkrussell756@gmail.com>
2021-02-04 07:53:58 -0500

    Added a general style scss page, added a scss page for the home container, added basic styling to the home container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import Container from 'react-bootstrap/Container'
+// Bootstrap components
+import Container from 'react-bootstrap/Container';
+
+// Stylesheet
+import './Home.scss';
 
 const Home = () => {
   return (
@@ -11,6 +15,19 @@ const Home = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </Container>
       </header>
+
+      <section className="about">
+        <Container>
+          <h2>About Kallick Consulting</h2>
+          <p className="about">I was thinking you could use this section to give a brief description of the kind of work you do as Kallick Consulting. Underneath you could list a few accomplishments you're looking to highlight. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat commodo sed egestas egestas fringilla phasellus. Et netus et malesuada fames ac turpis egestas integer eget.</p>
+          <ul>
+            <li>Generic accomplishment #1</li>
+            <li>Generic accomplishment #2</li>
+            <li>And so forth</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </ul>
+        </Container>
+      </section>
     </div>
   )
 }
