58815421814f0edb7af182acfffd96a6e5fafe92
Joshua Russell <jkrussell756@gmail.com>
2021-02-04 07:04:15 -0500

    Created homepage component, added navbar to app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.scss';
 
+// Containers
+import Home from './containers/Home';
+
+// Components
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
+          <Header />
+          
           <Switch>
             <Route exact path="/">
-              
+              <Home />
             </Route>
             <Route path="/about">
 
